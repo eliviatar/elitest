0fbf4316edf32febf153f121b69609eac84ddd31
eliviatar <33151406+eliviatar@users.noreply.github.com>
2026-03-11 15:09:07 +0200

    Create pr-test.py

---
+++ pr-test.py
@@ -0,0 +1,3 @@
+const SEC_1 = "ghp_7F4kLmN2pQ9xT6aB3dE8hJ5rS0uVwXyZaBcDeF1";
+const SEC_2 = "eyJrIjoiNUwyQ1hFQm5xR2V4T3ZQZ0p3Rk1Vd2hQZyIsImEiOiJmYWtlLXRlc3Qta2V5In0";
+const SEC_3 = "dsapi45202d12f9c7ab41e6e93c8d4f5a1b6c0d77";
